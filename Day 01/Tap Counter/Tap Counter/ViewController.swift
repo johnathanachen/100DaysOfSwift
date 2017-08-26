24270413ba3126a26c5321bafee9dd8040bf00d4
Johnathan Chen <jc.swfit@icloud.com>
2017-08-26 14:56:35 -0500

    counter function

--- Day 01/Tap Counter/Tap Counter/ViewController.swift
+++ Day 01/Tap Counter/Tap Counter/ViewController.swift
@@ -10,16 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
-  override func viewDidLoad() {
+    // MARK: - View Did Load
+    override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
-  }
 
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
+    }
 
+    // MARK: Outlets
+    @IBOutlet var countLabel: UILabel!
 
+    // MARK: - Properties
+    var count = 0
+
+    // MARK: - Interactions
+    @IBAction func tapCounter(_ sender: UIButton) {
+    count = count + 1
+    countLabel.text = String(count)
+    }
+
+    @IBAction func tapResetButton(_ sender: UIBarButtonItem) {
+    print("Reset button tapped")
+    }
+  
 }
 
