e5502a5f3ee50620df107f11bf46a0939a9060fc
Johnathan Chen <jc.swfit@icloud.com>
2017-08-28 02:34:39 -0500

    Add done button to keyboard

--- Day 04/Tip Calculator/Tip Calculator/ViewController.swift
+++ Day 04/Tip Calculator/Tip Calculator/ViewController.swift
@@ -14,9 +14,45 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         subtotalTextField.becomeFirstResponder()
+        createToolbar()
     }
 
     // MARK: - Outlets
     @IBOutlet weak var subtotalTextField: UITextField!
+    
+    // MARK: - Functions
+    func createToolbar() {
+        let toolbar = UIToolbar()
+        toolbar.barStyle = .default
+        toolbar.sizeToFit()
+        
+        // create barbuttonitems
+        let doneButton = UIBarButtonItem(
+            barButtonSystemItem: .done,
+            target: self,
+            action: #selector(dismissKeyboard))
+        
+        let flexibleSpace = UIBarButtonItem(
+            barButtonSystemItem: .flexibleSpace,
+            target: self,
+            action: nil)
+        
+        let rightSpace = UIBarButtonItem(
+            barButtonSystemItem: .fixedSpace,
+            target: self,
+            action: nil)
+        
+        rightSpace.width = 12
+        
+        // add item to toolbar
+        toolbar.setItems([flexibleSpace, doneButton, rightSpace], animated: true)
+        
+        // add toolbar to keyboard
+        subtotalTextField.inputAccessoryView = toolbar
+    }
+    
+    func dismissKeyboard() {
+        subtotalTextField.resignFirstResponder()
+    }
 }
 
