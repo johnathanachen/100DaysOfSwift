c6dff74d16046222882be74cae9485cb715e553c
Johnathan Chen <jc.swfit@icloud.com>
2017-09-04 11:56:10 -0700

    delete function from UItable

---
+++ Day 08/BasicTableView/BasicTableView/Model.swift
@@ -0,0 +1,14 @@
+//
+//  Model.swift
+//  BasicTableView
+//
+//  Created by Johnathan Chen on 9/4/17.
+//  Copyright © 2017 JCSwifty. All rights reserved.
+//
+
+import Foundation
+
+class Model {
+    var data = ["Milk", "Apples", "Ham", "Eggs"]
+    var cloudData = ["Pancakes", "Orange Juice", "Milk", "Apples", "Ham", "Waffles", "Eggs"]
+}
