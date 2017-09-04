af2b451787afd1e720baccb83d57a691dfbe3a85
Johnathan Chen <jc.swfit@icloud.com>
2017-09-04 11:38:10 -0700

    table refresh on pull

--- Day 08/BasicTableView/BasicTableView/GroceriesTableViewController.swift
+++ Day 08/BasicTableView/BasicTableView/GroceriesTableViewController.swift
@@ -13,10 +13,24 @@ class GroceriesTableViewController: UITableViewController {
     // MARK: - View Did Load
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.navigationItem.leftBarButtonItem = self.editButtonItem
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: nil)
+        self.refreshControl = pullToRefreashControl
+        pullToRefreashControl.addTarget(self, action: #selector(refreshTable), for: .valueChanged)
     }
 
     // MARK: - Properties
-    var groceries = ["Milk", "Apples", "Ham", "Eggs"]
+    var data = ["Milk", "Apples", "Ham", "Eggs"]
+    var cloudData = ["Pancakes", "Orange Juice", "Milk", "Apples", "Ham", "Waffles", "Eggs"]
+    let pullToRefreashControl = UIRefreshControl()
+    
+    // MARK: - Functions
+    @objc func refreshTable() {
+        data = cloudData
+        self.tableView.reloadData()
+        pullToRefreashControl.endRefreshing()
+    }
 
     // MARK: - Table view data source
     override func numberOfSections(in tableView: UITableView) -> Int {
@@ -24,14 +38,14 @@ class GroceriesTableViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return groceries.count
+        return data.count
     }
 
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "groceriesCell", for: indexPath)
 
-        cell.textLabel?.text = groceries[indexPath.row]
+        cell.textLabel?.text = data[indexPath.row]
 
         return cell
     }
