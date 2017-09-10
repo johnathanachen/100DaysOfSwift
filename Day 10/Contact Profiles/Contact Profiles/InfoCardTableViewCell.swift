7d92be87649ac10e48b1524cbec710df85142873
Johnathan Chen <jc.swfit@icloud.com>
2017-09-10 13:28:44 -0700

    complete UI info card

---
+++ Day 10/Contact Profiles/Contact Profiles/InfoCardTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  InfoCardTableViewCell.swift
+//  Contact Profiles
+//
+//  Created by Johnathan Chen on 9/10/17.
+//  Copyright © 2017 JCSwifty. All rights reserved.
+//
+
+import UIKit
+
+class InfoCardTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Day 10/Contact Profiles/Contact Profiles/Model.swift
@@ -0,0 +1,20 @@
+//
+//  Model.swift
+//  Contact Profiles
+//
+//  Created by Johnathan Chen on 9/10/17.
+//  Copyright © 2017 JCSwifty. All rights reserved.
+//
+
+import Foundation
+
+let model = Model()
+
+class Model {
+    var recent = [
+    [
+        "name": "keith hansen",
+        "mobile": "keith hansen",
+        "name": "keith hansen",
+    ]
+}
