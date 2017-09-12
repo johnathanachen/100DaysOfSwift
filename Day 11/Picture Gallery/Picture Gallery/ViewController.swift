cd7de116d217b0ce4b12bad1d75b76ae6acd794e
Johnathan Chen <jc.swfit@icloud.com>
2017-09-11 19:30:36 -0700

    set location

--- Day 11/Picture Gallery/Picture Gallery/ViewController.swift
+++ Day 11/Picture Gallery/Picture Gallery/ViewController.swift
@@ -7,19 +7,53 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    // MARK: - View Did Load
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        locationManager.delegate = self
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    // MARK: - Outlets
+    @IBOutlet weak var locationLabel: UILabel!
+    
+    // MARK: - Interactions
+    @IBAction func tapSetLocationButton(_ sender: UIButton) {
+        getLocation()
     }
+    
+    // MARK: - Location
+    let locationManager = CLLocationManager()
 
-
+    // MARK: Location Functions
+    func getLocation() {
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.desiredAccuracy = kCLLocationAccuracyKilometer
+        locationManager.startUpdatingLocation()
+    }
+    
+    // MARK: CLLocation Manager Delegate
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        locationManager.stopUpdatingLocation()
+        let geocoder = CLGeocoder()
+        
+        geocoder.reverseGeocodeLocation(manager.location!) { (placemarks, error) in
+            if let placemarksData = placemarks {
+                let locationData = placemarksData[0]
+                // San Francisco, CA zip, United States
+                let city = locationData.locality!
+                let state = locationData.administrativeArea!
+                let zipCode = locationData.postalCode!
+                let country = locationData.country!
+                let location = "\(city), \(state), \(zipCode), \(country)"
+                
+                self.locationLabel.text = location
+                self.locationLabel.textColor = .black
+            }
+        }
+    }
 }
 
