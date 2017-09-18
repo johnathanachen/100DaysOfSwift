629491e8dd47f446d87e98acc73d5efcedf57df3
Johnathan Chen <jc.swfit@icloud.com>
2017-09-17 17:59:13 -0700

    Update table with changes in core data

--- Day 11/Picture Gallery/Picture Gallery/TimelineTableViewController.swift
+++ Day 11/Picture Gallery/Picture Gallery/TimelineTableViewController.swift
@@ -35,6 +35,40 @@ class TimelineTableViewController: UITableViewController, NSFetchedResultsContro
             fatalError("Failed to initialize FetchedResultsController: \(error)")
         }
     }
+    
+    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.beginUpdates()
+    }
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange sectionInfo: NSFetchedResultsSectionInfo, atSectionIndex sectionIndex: Int, for type: NSFetchedResultsChangeType) {
+        switch type {
+        case .insert:
+            tableView.insertSections(IndexSet(integer: sectionIndex), with: .fade)
+        case .delete:
+            tableView.deleteSections(IndexSet(integer: sectionIndex), with: .fade)
+        case .move:
+            break
+        case .update:
+            break
+        }
+    }
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+        switch type {
+        case .insert:
+            tableView.insertRows(at: [newIndexPath!], with: .fade)
+        case .delete:
+            tableView.deleteRows(at: [indexPath!], with: .fade)
+        case .update:
+            configureCell(cell: tableView.cellForRow(at: indexPath!)!, indexPath: indexPath!)
+        case .move:
+            tableView.moveRow(at: indexPath!, to: newIndexPath!)
+        }
+    }
+    
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.endUpdates()
+    }
 
 
     // MARK: - Table view data source
