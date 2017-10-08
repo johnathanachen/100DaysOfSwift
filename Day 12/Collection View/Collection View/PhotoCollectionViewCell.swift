25722a79feca4d504ef57f788fdca254a2db9e0b
Johnathan Chen <johnathanchenn@outlook.com>
2017-10-08 13:16:36 -0700

    photo collection

---
+++ Day 12/Collection View/Collection View/PhotoCollectionViewCell.swift
@@ -0,0 +1,15 @@
+ //
+//  PhotoCollectionViewCell.swift
+//  Collection View
+//
+//  Created by Johnathan Chen on 10/8/17.
+//  Copyright © 2017 JCSwifty. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    
+    // MARK: - outlets 
+    @IBOutlet weak var imageView: UIImageView!
+ }
