3df9dc5c5d30bcddc535c5cd5da6113836379d9f
Johnathan Chen <johnathanchenn@outlook.com>
2017-10-16 16:44:43 -0700

    alert view and action sheet completed

--- Day 12/Collection View/Collection View/PostViewController.swift
+++ Day 12/Collection View/Collection View/PostViewController.swift
@@ -34,5 +34,45 @@ class PostViewController: UIViewController {
     @IBOutlet weak var postLabel: UILabel!
     @IBOutlet weak var tagsLabel: UILabel!
     
+    // MARK: - Interactions
+    @IBAction func tapOptionsButton(_ sender: UIBarButtonItem) {
+
+        showActionSheet()
+    }
+    
+    // MARK: - Functions
+    func showAlert() {
+        let alertController = UIAlertController(title: "Alert View", message: "Hello", preferredStyle: .alert)
+        let confirm = UIAlertAction(title: "OK", style: .default, handler: nil)
+        let cancel = UIAlertAction(title: "Cancel", style: .destructive, handler: nil)
+        
+        alertController.addAction(confirm)
+        alertController.addAction(cancel)
+        
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    func showActionSheet() {
+        let alertController = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        
+        let report = UIAlertAction(title: "Report", style: .destructive, handler: nil)
+        let share = UIAlertAction(title: "Share", style: .default, handler: nil)
+        let saveImage = UIAlertAction(title: "Save Image", style: .default, handler: nil)
+        let copyLink = UIAlertAction(title: "Copy Link", style: .default, handler: nil)
+        let cancel = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        
+        let actions = [report, share, saveImage, copyLink, cancel]
+        
+        for action in actions {
+            alertController.addAction(action)
+        }
+        
+        
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    
+    
+    
 
 }
